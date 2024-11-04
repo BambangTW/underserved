1d0e717d9e322902a994375752a1843be06e94f7
BAMBANG TRIARTHO WIBOWO <bambangwibowo.205022@mhs.its.ac.id>
2024-11-04 13:54:53 +0700

    remove GL_TEST

--- test/tb.v
+++ test/tb.v
@@ -32,12 +32,6 @@ module tb ();
   // Replace tt_um_example with your module name:
   tt_um_underserved user_project (
 
-      // Include power ports for the Gate Level test:
-// `ifdef GL_TEST
-//       .VPWR(1'b1),
-//       .VGND(1'b0),
-// `endif
-
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out ({uo_out[7:5], gpio4, gpio3, gpio2, gpio1, gpio0}),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
